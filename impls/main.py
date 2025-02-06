34baacfacd557d8c7e83dc404059b4017ac8953a
annazhou <zn.anna@gmail.com>
2025-02-06 10:58:34 -0800

    comment wandb

--- impls/main.py
+++ impls/main.py
@@ -13,7 +13,7 @@ from absl import app, flags
 from ml_collections import config_flags
 
 from agents import agents
-from envs.env_utils import make_env_and_datasets
+from utils.env_utils import make_env_and_datasets
 from utils.datasets import Dataset, ReplayBuffer
 from utils.evaluation import evaluate, flatten
 from utils.flax_utils import restore_agent, save_agent
