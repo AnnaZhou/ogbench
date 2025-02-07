6fbc49e852c8ce80ee37bab2efec11a52888f2ee
annazhou <zn.anna@gmail.com>
2025-02-07 09:59:46 -0800

    comment wandb

--- impls/main.py
+++ impls/main.py
@@ -14,7 +14,7 @@ from ml_collections import config_flags
 
 from agents import agents
 from envs.env_utils import make_env_and_datasets
-from utils.datasets import Dataset, ReplayBuffer
+from utils.fql_datasets import Dataset, ReplayBuffer
 from utils.evaluation import evaluate, flatten
 from utils.flax_utils import restore_agent, save_agent
 from utils.log_utils import CsvLogger, get_exp_name, get_flag_dict#, get_wandb_video, setup_wandb
